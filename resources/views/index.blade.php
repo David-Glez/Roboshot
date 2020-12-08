834c5271f75416b3500cce3b12356cb9354499df
David-Glez <david.gonzalez_isc@outlook.com>
2020-12-08 14:10:59 -0600

    Secure Asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>RoboShot</title>
-    <!--<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">-->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
     <link rel="shortcut icon" href="{{ asset('images/beer.svg')}}">
 </head>
 
@@ -17,6 +17,6 @@
     
 </body>
 
-<script src="{{ asset('js/app.js')}}"></script>
+<script src="{{ secure_asset('js/app.js')}}"></script>
 
 </html>
